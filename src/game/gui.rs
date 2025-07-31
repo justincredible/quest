04115fb41a1893566d629ee5b0a3b2f3cf12523d
Justin Symonds <justinjfsymonds@hotmail.com>
2025-07-31 00:58:46 -0700

    Refactor game gui

--- src/game/gui.rs
+++ src/game/gui.rs
@@ -17,6 +17,21 @@ impl Game {
 
 impl crate::app::Gui for Game {
     fn gui(&mut self, context: &egui::Context) {
+	egui::SidePanel::left("Menu").resizable(false).show(context, |ui| {
+	    let layout = egui::Layout {
+		main_dir: egui::Direction::BottomUp,
+		cross_align: egui::Align::Center,
+		cross_justify: true,
+		..Default::default()
+	    };
+	    ui.with_layout(layout, |ui| {
+		if ui.button("quit").clicked() {
+		    *self = Game::Exited;
+		}
+	    });
+	});
+	egui::TopBottomPanel::bottom("Minibuffer").resizable(false).show(context, |ui| {
+	});
         egui::CentralPanel::default().show(context, |ui| {
             match self {
                 Game::Loaded(None) => {
@@ -27,8 +42,4 @@ impl crate::app::Gui for Game {
             }
         });
     }
-}
-
-fn ui(ui: &mut egui::Ui) {
-
 }
